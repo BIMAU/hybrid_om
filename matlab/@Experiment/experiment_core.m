b93674230405d2b8d3d3a72c9fd54ce781088ac7
Erik <t.e.mulder@rug.nl>
2021-06-28 16:35:17 +0200

    reimplemented most of experiment_core

--- matlab/@Experiment/experiment_core.m
+++ matlab/@Experiment/experiment_core.m
@@ -1,2 +1,132 @@
 function [predY, testY, err, esnX] = experiment_core(self)
+% Core routine for running an experiment
+%   predY:  full dimensional predictions
+%   testY:  full dimensional truths
+
+% we use the time step size of  the imperfect model
+    dt  = self.data.dt_imp;
+
+    % sample/state vector dimension, possibly reduced
+    dim = size(self.VX,1);
+
+    % sanity check
+    assert(dim == size(self.modes.V,2))
+
+    % three different experiments based on settings
+    hybrid     = logical( (self.esn_on   == true ) && ...
+                          (self.model_on == true ) );
+    esn_only   = logical( (self.esn_on   == true ) && ...
+                          (self.model_on == false) );
+    model_only = logical( (self.esn_on   == false) && ...
+                          (self.model_on == true ) );
+
+    if hybrid
+        self.print('Create input/output data for hybrid ESN\n');
+        exp_type = 'hybrid';
+        U = [self.VX(:, 1:end-1); self.VPhi(:, 1:end-1)];
+        Y = [self.VX(:, 2:end)];
+    elseif esn_only
+        self.print('Create input/output data for standalone ESN\n')
+        exp_type = 'esn_only';
+        U = [self.VX(:, 1:end-1)];
+        Y = [self.VX(:, 2:end)];
+    elseif model_only
+        exp_type = 'model_only';
+    end
+
+    if self.esn_on
+        assert(self.train_range(end)+1 == self.test_range(1), ...
+               'incompatible train and test range');
+        trainU = U(:, self.train_range)';
+        trainY = Y(:, self.train_range)';
+        clear U Y ; % clean up
+    end
+
+    % full dimensional output testing
+    if self.testing_on
+        testY = self.VX(:, 2:end);
+        testY = testY(:, self.test_range)';
+    else
+        testY = [];
+    end
+    
+    Npred = numel(self.test_range); % number of prediction steps
+    predY = zeros(Npred, dim); % full dimensional predictions
+    err   = zeros(Npred, 1); % error array
+    esnX  = [];       % esn state snapshots
+
+    % initial state for the predictions
+    init_idx = self.train_range(end)+1;
+    yk = self.VX(:, init_idx);
+
+    self.print('initialization index: %d\n', init_idx);
+
+    clear self.VX self.VPhi;
+    
+    if self.esn_on
+        % enable hybrid input design
+        if hybrid
+            assert(size(trainU,2) == 2*dim, ...
+                   'inconsistent hybrid input dimension');
+            self.esn_pars.feedThrough = true;
+            self.esn_pars.ftRange     = dim+1:2*dim;
+        end
+        
+        % create ESN, train the ESN and save the final state
+        esn = ESN(self.esn_pars.Nr, size(trainU,2), size(trainY,2));
+        esn.setPars(self.esn_pars);
+        esn.initialize;
+        esn.train(trainU, trainY);
+        esn_state = esn.X(end,:);
+        esnX = esn.X;
+        
+        clear trainU trainY
+        
+        verbosity = 1;
+        for i = 1:Npred
+            % model prediction of next time step
+            [Pyk, Nk] = self.model.step(yk, dt);
+            
+            if model_only
+                % result is not adapted
+                yk = Pyk;
+            else
+                % create an input vector for the ESN
+                if hybrid
+                    u_in = [self.modes.V' * yk(:); self.modes.V' * Pyk(:)]';
+                elseif esn_only
+                    u_in = [self.modes.V' * yk(:)];
+                else
+                    error('incorrect model config')
+                end
+                
+                u_in      = esn.scaleInput(u_in);
+                esn_state = esn.update(esn_state, u_in)';
+                u_out     = esn.apply(esn_state, u_in);
+                u_out     = esn.unscaleOutput(u_out);
+
+                % transform ESN prediction back to the state space 
+                yk = self.modes.V * u_out(:);
+                
+                % combine ESN prediction with model prediction
+                % yk = yk + Vc*(Vc'*Pyk); % TODO
+            end
+            
+            % store result
+            predY(i,:) = yk;
+            
+            % check stopping criterion
+            stop = false;
+            if self.testing_on
+                [stop, err(i)] = ...
+                    self.stopping_criterion(predY(i,:), testY(i,:));
+            end
+            
+            if mod(i,verbosity) == 0
+                self.print(['prediction step %4d/%4d, Newton iterations %d,',...
+                            'error %1.2e, %s\n'], ...
+                           i, Npred, Nk, err(i), exp_type);
+            end
+        end
+    end
 end
--- matlab/main_KS.m
+++ matlab/main_KS.m
@@ -16,7 +16,7 @@ dgen.dimension = '1D';
 dgen.build_grid_transfers('periodic');
 
 % generate perfect model transient
-dgen.T = 100;         % total time
+dgen.T = 100;        % total time
 dgen.dt_prf = 0.25;  % perfect model time step
 dgen.trunc = 10;     % truncate period
 dgen.generate_prf_transient();
@@ -28,6 +28,8 @@ dgen.generate_imp_predictions();
 
 % create experiment class
 expObj = Experiment(dgen);
+expObj.shifts = 2;
+expObj.reps = 1;
 
 % adjust hyperparam defaults
 expObj.set_default_hyp('ReservoirSize', 1000);
@@ -35,11 +37,8 @@ expObj.set_default_hyp('BlockSize', 16);
 expObj.set_default_hyp('TrainingSamples', 50);
 
 % set experiments
-expObj.add_experiment('ReservoirSize', [500, 1000, 2000]);
-expObj.add_experiment('BlockSize', [1, 4, 16]);
+expObj.add_experiment('ReservoirSize', [500, 1000]);
+% expObj.add_experiment('BlockSize', [1, 4, 16]);
 
 % run experiments
 expObj.run();
-
-VX   = expObj.VX;
-VPhi = expObj.VPhi;
