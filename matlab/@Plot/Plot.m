a588610269e0b139cd672c90812a3f23aec97ea7
Erik <t.e.mulder@rug.nl>
2021-06-30 19:53:24 +0200

    Started work on plotting class

---
+++ matlab/@Plot/Plot.m
@@ -0,0 +1,9 @@
+classdef Plot < handle
+% class that handles (box)plotting of the grid search experiment
+    
+    properties (Access = public)
+    end
+    
+    methods (Access = public)
+    end
+end
