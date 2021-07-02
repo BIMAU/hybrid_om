d6f6fbe9f5ffccf703a1e7a9cdbc05b14ba514f0
Erik <t.e.mulder@rug.nl>
2021-07-02 20:49:10 +0200

    forget to add this to repo

---
+++ matlab/@Experiment/stopping_criterion.m
@@ -0,0 +1,9 @@
+function [stop_flag, err] = stopping_criterion(self, predY, testY)
+    err  = self.nrmse(predY(:), testY(:));
+
+    % err = norm(testY(:) - predY(:)) / norm(testY(:),2);
+    stop_flag = false;
+    if (err > self.err_tol) && ~strcmp(self.store_state, 'all')
+        stop_flag = true;
+    end
+end
