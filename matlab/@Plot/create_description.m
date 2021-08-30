c3aa214484f9524c657ed64479103eb543fa2d94
Erik <t.e.mulder@rug.nl>
2021-08-30 21:09:44 +0200

    added padding to descriptor box

--- matlab/@Plot/create_description.m
+++ matlab/@Plot/create_description.m
@@ -7,7 +7,7 @@ function [description] = create_description(self, mdat)
     for i = 1:numel(par_names)
         value_start = mdat.hyp_range(i, 1);
         value_end   = mdat.hyp_range(i, end);
-        description = sprintf('%s\n%18s: %1.1d-%1.1d', description, ...
+        description = sprintf('%s\n%21s: %1.1d-%1.1d', description, ...
                               par_names{i}, value_start, value_end);
     end
 end
