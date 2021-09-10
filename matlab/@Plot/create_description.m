f872b79d57448876f044b346cade5fb3932516a6
Erik <t.e.mulder@rug.nl>
2021-09-10 20:46:49 +0200

    ESN/Model description is now much more complicated...
    Needs some work to get a nice output in the plots.

--- matlab/@Plot/create_description.m
+++ matlab/@Plot/create_description.m
@@ -2,8 +2,8 @@ function [description] = create_description(self, mdat)
     par_names = fieldnames(mdat.hyp);
     default = 1;
 
-    description = sprintf('%18s: %1.1d', 'ESN', mdat.esn_on);
-    description = sprintf('%s\n%18s: %1.1d', description, 'Model', mdat.model_on);
+    description = sprintf('');
+
     for i = 1:numel(par_names)
         value_start = mdat.hyp_range(i, 1);
         value_end   = mdat.hyp_range(i, end);
