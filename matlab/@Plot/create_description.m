862f3ce5e8f71c8bb6a36974f942ec8b6d2a2a7d
Erik <t.e.mulder@rug.nl>
2021-09-14 14:46:03 +0200

    created readable description

--- matlab/@Plot/create_description.m
+++ matlab/@Plot/create_description.m
@@ -5,9 +5,18 @@ function [description] = create_description(self, mdat)
     description = sprintf('');
 
     for i = 1:numel(par_names)
-        value_start = mdat.hyp_range(i, 1);
-        value_end   = mdat.hyp_range(i, end);
-        description = sprintf('%s\n%21s: %1.1d-%1.1d', description, ...
-                              par_names{i}, value_start, value_end);
+        has_opts = isfield(mdat.hyp.(par_names{i}), 'opts');
+
+        if has_opts
+            value_start = mdat.hyp.(par_names{i}).opts{mdat.hyp_range(i, 1)};
+            value_end   = mdat.hyp.(par_names{i}).opts{mdat.hyp_range(i, end)};
+            description = sprintf('%s\n%21s: %s-%s', description, ...
+                                  par_names{i}, value_start, value_end);
+        else
+            value_start = mdat.hyp_range(i, 1);
+            value_end   = mdat.hyp_range(i, end);
+            description = sprintf('%s\n%21s: %1.1d-%1.1d', description, ...
+                                  par_names{i}, value_start, value_end);
+        end
     end
 end
