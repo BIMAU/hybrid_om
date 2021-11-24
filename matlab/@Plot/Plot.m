10f3346fed098aeaf83220c773674929b4956e8b
Erik <t.e.mulder@rug.nl>
2021-11-24 21:59:00 +0100

    change access

--- matlab/@Plot/Plot.m
+++ matlab/@Plot/Plot.m
@@ -3,34 +3,34 @@ classdef Plot < handle
 % dir: it requires a directory dir
 
     properties (Access = public)
-        
+
         % location of experiment results
         dir;
 
         % enable or disable legend
         legend = true;
-        
+
         % flag to add hyperparam settings in plot
         description = false;
-        
+
         % enable/disable title
         show_title = true;
-        
+
         % scales the amount of samples
         scaling = 1.0;
-        
+
         % overwrites ylabel prescribed in data
         ylab = {};
 
         % overwrites xlabel prescribed in data
         xlab = {};
-        
+
         % enable/disable grid
         grid = true;
-        
+
         % plot the mean together with the boxplot
         plot_mean = true;
-        
+
         % show the whole dataset in the background
         plot_scatter = true;
     end
@@ -45,27 +45,27 @@ classdef Plot < handle
         [nums, mdat, preds, truths, s] = get_qg_transient_data(self, opts);
         [stats] = get_qg_statistics(self, qg, data, opts);
         [] = movie_qg(self, data, opts);
-    end
-
-    methods (Access = private)
         % gather data from output .mat file
         [errs, nums, pids, ...
          metadata, predictions, ...
          truths] = gather_data(self, varargin);
 
         [description] = create_description(self, mdat);
-        
+    end
+
+    methods (Access = private)
+
         [f] = my_boxplot(self, varargin);
-        
+
         function [out] = compute_qg_enstrophy(self, field)
         % assume QG ordering
-            nun = 2;    
-            out = sum(field(1:nun:end).^2);    
+            nun = 2;
+            out = sum(field(1:nun:end).^2);
         end
-        
+
         function [out] = compute_qg_energy(self, field)
         % assume QG ordering
-            nun = 2;    
+            nun = 2;
             out = sum(field(1:nun:end).*field(2:nun:end));
         end
     end
