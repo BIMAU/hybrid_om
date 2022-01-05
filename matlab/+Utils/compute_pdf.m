3a020939e48eeddc193f426a25cc64b4b0caa854
Erik <t.e.mulder@rug.nl>
2022-01-05 11:20:26 +0100

    implemented dkl

--- matlab/+Utils/compute_pdf.m
+++ matlab/+Utils/compute_pdf.m
@@ -1,4 +1,4 @@
-function [hc, edg] = compute_pdf(series, bins, varargin)
-    [hc, edg] = histcounts(series, bins, 'normalization', 'pdf');
-    edg = (edg(1:end-1)+edg(2:end))/2;
+function [hc, cnt] = compute_pdf(series, bins, varargin)
+    [hc, edg] = histcounts(series, bins, varargin{:});
+    cnt = (edg(1:end-1)+edg(2:end))/2;
 end
