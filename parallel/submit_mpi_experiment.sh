9f606df6d9e2362f1a14c43255e244199665a745
Erik@Peregrine <t.e.mulder@rug.nl>
2022-02-24 14:06:37 +0100

    submit script, commented different cases

--- parallel/submit_mpi_experiment.sh
+++ parallel/submit_mpi_experiment.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-#SBATCH --time=36:00:00
-#SBATCH --ntasks=1
+##For KS experiments:
+##SBATCH --time=1:30:00
+##SBATCH --ntasks=50
+##SBATCH --mem-per-cpu=20GB
+
+# For QG experiments:
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=50
 #SBATCH --mem-per-cpu=16GB
+
+### A short test (QG)
+##SBATCH --ntasks=5
+##SBATCH --mem-per-cpu=13GB
 ##SBATCH --partition=short
 
+#### A short test (QG)
+##SBATCH --ntasks=90
+##SBATCH --mem-per-cpu=10GB
+##SBATCH --partition=short
+
+##### A short test (KS)
+##SBATCH --ntasks=50
+##SBATCH --mem-per-cpu=20GB
+##SBATCH --partition=short
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: submit_mpi_experiment.sh <output_exec>"
@@ -16,4 +35,4 @@ module load MATLAB/2018a
 
 echo "running ./interface" $1
 
-srun -n 1 ./interface $1
+srun -n 50 ./interface $1
