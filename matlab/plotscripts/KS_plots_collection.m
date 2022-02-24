b52d8c6c83083b320f75f9bf88e0c3253feaf32e
Erik <t.e.mulder@rug.nl>
2022-02-24 14:15:26 +0100

    KS plotting

--- matlab/plotscripts/KS_plots_collection.m
+++ matlab/plotscripts/KS_plots_collection.m
@@ -2,15 +2,14 @@
 
 addpath('../')
 
- exp_dirs = {'Pathak2018repl/NR_200-6400_MC_1-8_parallel_param_1.00e+00/', ...
-             'Pathak2018repl/NR_200-6400_MC_1-8_parallel_param_1.00e-01/', ...
-             'Pathak2018repl/NR_200-6400_MC_1-8_parallel_param_1.00e-02/' ...
-             'KS_GridExp/NR_200-6400_MC_1-8_parallel_param_0.00e+00'};
-
+exp_dirs = {'Pathak2018repl/NR_50-6400_MC_1-8_parallel_param_1.00e+00/', ...
+            'Pathak2018repl/NR_50-6400_MC_1-8_parallel_param_1.00e-01/', ...
+            'Pathak2018repl/NR_50-6400_MC_1-8_parallel_param_1.00e-02/' ...
+            'KS_GridExp/NR_50-6400_MC_1-8_parallel_param_0.00e+00'};
 
 base_dir = '~/Projects/hybrid_om/data/experiments/';
-set(groot,'defaultAxesTickLabelInterpreter','latex'); 
-
+set(groot,'defaultAxesTickLabelInterpreter','latex');
+nums=[];
 for k = 1:numel(exp_dirs)
     figure(k)
     clf
@@ -26,22 +25,30 @@ for k = 1:numel(exp_dirs)
     p.scaling = 0.25 * 0.07;
     p.ylab = 'Valid time';
     p.xlab = '$N_r$';
-    p.style = {'.', '-', '-'};
-    p.msize = {15, 12};
+    p.style = {'.', '.-', {':', '-',':'}};
+    p.msize = {15, 15};
+    p.plot_q_conn = [1,1,1];
 
-
-    [nums, mdat, preds, truths, f] = p.plot_experiment(false);
+    [nums{k}, mdat, preds, truths, f] = p.plot_experiment(false);
 
     output_dir = ['~/Projects/doc/mlqg/figs/', exp_dirs{k}];
     eval(['mkdir ', output_dir]);
 
     Utils.create_description(mdat)
-
-    if k >= 3
+    if k == 3
         % create legend plot
-        lgd = legend([f{:}], 'imperfect model', 'ESN', 'ESNc', 'DMDc', 'correction only', 'ESN+DMDc', 'location', 'northeastoutside', 'interpreter', 'latex', 'orientation','vertical');
+        lgd = legend([f{:}], 'imperfect model', 'ESN', 'ESNc', 'DMDc', ...
+                     'correction only', 'ESN+DMDc', 'location', 'northeastoutside', ...
+                     'interpreter', 'latex', 'orientation','vertical');
         lgd.NumColumns = 1;
         exportfig([output_dir, '/fig.eps'], 16, [22,14])
+    elseif k == 4
+        % create legend plot
+        lgd = legend([f{:}], 'imperfect model', 'ESN', 'ESNc', 'DMDc', ...
+                     'correction only', 'ESN+DMDc', 'location', 'northwest', ...
+                     'interpreter', 'latex', 'orientation','vertical');
+        lgd.NumColumns = 1;
+        exportfig([output_dir, '/fig.eps'], 16, [18,14])
     else
         exportfig([output_dir, '/fig.eps'], 16, [18,14])
     end
