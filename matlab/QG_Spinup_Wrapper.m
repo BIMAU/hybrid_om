9fb7e7984c2aa7e020b6b88f6ab86285723364b6
Erik <t.e.mulder@rug.nl>
2023-06-18 17:37:43 +0200

    change to habrok setup

--- matlab/QG_Spinup_Wrapper.m
+++ matlab/QG_Spinup_Wrapper.m
@@ -2,17 +2,12 @@ function QG_Spinup_Wrapper(varargin)
     [pid, procs] = Utils.input_handling(nargin, varargin);
     Utils.add_paths();
 
-    % local machine
-    % start_solutions = ['/home/erik/Projects/hybrid_om/',...
-    %                    'data/QGmodel/starting_solutions'];
-
-    % habrok
+    % both local and on habrok
     start_solutions = ['~/Projects/hybrid_om/', ...
                        'data/QGmodel/starting_solutions'];
 
     % local machine
     % data_dir = '/home/erik/Projects/hybrid_om/data';
-
 
     % habrok
     data_dir = '/scratch/p267904/Projects/hybrid_om/data';
@@ -26,12 +21,14 @@ function QG_Spinup_Wrapper(varargin)
 
     my_inds = Utils.my_indices(pid, procs, N_ensemble);
 
+    % loop for output only
     for i = my_inds
         spinup_name = sprintf('return_from_%s/spinup_%d_', basename, i);
         fprintf('pid %d procs %d, %s \n', pid, procs, ...
                 spinup_name);
     end
 
+    % loop to call spinup routine
     for i = my_inds
         init_name = sprintf('%s/%s_prediction_%d.mat', ...
                             start_solutions, basename, i);
